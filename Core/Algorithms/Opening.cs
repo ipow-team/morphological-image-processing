71bd7e813d1774a5c5c71dd78ec03e2bf67606eb
Justyna Zieniewicz <235735@student.pwr.edu.pl>
2020-11-18 17:13:22 +0100

    add opening and closing algorithms

---
+++ Core/Algorithms/Opening.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+namespace MorphologicalImageProcessing.Core.Algorithms
+{
+    class Opening : MorphologicalAlgorithm<DefaultMorphologicalAlgorithmConfiguration>
+    {
+        protected override Bitmap Apply(Bitmap image, DefaultMorphologicalAlgorithmConfiguration configuration)
+        {
+            return DrawEdges(image, configuration);
+        }
+
+        public override string GetName()
+        {
+            return "Opening";
+        }
+
+        public Bitmap DrawEdges(Bitmap image, DefaultMorphologicalAlgorithmConfiguration configuration)
+        {
+            Erosion erosion = new Erosion();
+            Dilatation dilatation = new Dilatation();
+            image = erosion.DrawEdges(image, configuration);
+            image = dilatation.DrawEdges(image, configuration);
+
+            return image;
+        }
+    }
+}
